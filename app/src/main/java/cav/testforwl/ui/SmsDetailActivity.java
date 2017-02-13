245787c033a8e2124b52260f4a8fd72dc4193225
CavInc <cavinc20@gmail.com>
2017-02-13 16:00:06 +0700

    Добавлена активность для просмотра полной информации о смс

---
+++ app/src/main/java/cav/testforwl/ui/SmsDetailActivity.java
@@ -0,0 +1,15 @@
+package cav.testforwl.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import cav.testforwl.R;
+
+public class SmsDetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms_detail);
+    }
+}
