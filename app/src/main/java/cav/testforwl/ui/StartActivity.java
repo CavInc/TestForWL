c9a85be3419953b8926eb235000e7a0b6b0f9716
CavInc <cavinc20@gmail.com>
2017-02-15 00:43:16 +0700

    Добавлено support-v4

--- app/src/main/java/cav/testforwl/ui/StartActivity.java
+++ app/src/main/java/cav/testforwl/ui/StartActivity.java
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 
